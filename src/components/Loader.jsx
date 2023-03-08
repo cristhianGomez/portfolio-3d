e9646ad525ca4d8709611c6a576d819380f8f886
cristhian <cristhian.gomez.7@gmail.com>
2023-03-07 20:33:55 -0500

    Feat: added Hero and About section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Html, useProgress } from '@react-three/drei'
 
 const Loader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="text-[12px] text-[#f1f1f1] font-bold mt-40">{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
